cc380d3a36cbdf2245a33fa724dc9e7c5e372cdd
Jonny Furness <jlsfurness@gmail.com>
2025-06-29 01:18:55 +0100

    Update lazy config

--- lua/jonny/lazy.lua
+++ lua/jonny/lazy.lua
@@ -1,22 +1,37 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "jonny.plugins" }, { import = "jonny.plugins.lsp"} }, {
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    notify = false,
-  },
+-- Setup Lazy and define LazyFile event
+require("lazy").setup({
+	{ import = "jonny.plugins" },
+	{ import = "jonny.plugins.lsp" },
+}, {
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
 })
+
+-- Define LazyFile event after lazy.nvim is loaded
+-- This allows plugins to use event = "LazyFile"
+local Event = require("lazy.core.handler.event")
+
+-- Create LazyFile event that triggers on file-related events
+Event.mappings.LazyFile = {
+	id = "LazyFile",
+	event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+}
+Event.mappings["User LazyFile"] = Event.mappings.LazyFile
